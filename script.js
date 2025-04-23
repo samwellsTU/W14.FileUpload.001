3a9c82d8f215b40fd1f25946e260bd8694cbb8cb
samwellsTU <111082260+samwellsTU@users.noreply.github.com>
2025-04-23 12:52:37 -0400

    4/23 inclass

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-
-
-
 const ctx = new AudioContext();
 const gain = ctx.createGain();
-let audiobuffer= null;
+let audiobuffer = null;
 
-gain.connect(ctx.destination)
+gain.connect(ctx.destination);
 
+document
+  .getElementById("fileUpload")
+  .addEventListener("change", async (event) => {
+    let file = event.target.files[0];
+    let arraybuf = await file.arrayBuffer();
+    audiobuffer = await ctx.decodeAudioData(arraybuf);
+    console.log(audiobuffer);
+  });
 
+document.getElementById("play").addEventListener("click", () => {
+  if (audiobuffer) {
+    let sourcenode = ctx.createBufferSource();
+    sourcenode.buffer = audiobuffer;
+    sourcenode.connect(gain);
+    sourcenode.start();
+  }
+});
